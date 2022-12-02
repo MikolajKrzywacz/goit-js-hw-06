ddcc0b45e43d04057f3c7236b181e25c59ca8f92
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2022-12-02 21:55:11 +0100

    Update task-06.js
    if statement added

---
+++ src/js/task-06.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input");
+const requiredNumLength = input.getAttribute("data-length");
+
+input.addEventListener("blur", () => {
+    if(input.value.length === requiredNumLength){
+        
+    }
+})
+
