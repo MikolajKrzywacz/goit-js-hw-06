3d2829d1a562336301bfefc6fcb30a5c2af26d15
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2022-12-02 16:39:53 +0100

    Update task-04.js
    finished task 04

---
+++ src/js/task-04.js
@@ -0,0 +1,23 @@
+let span = document.querySelector("#value");
+let counterValue = parseInt(document.querySelector("#value").textContent);
+console.log(typeof counterValue);
+
+const buttonDecrement = document.querySelector(
+  'button[data-action="decrement"]'
+);
+const buttonIncrement = document.querySelector(
+  'button[data-action="increment"]'
+);
+
+const decrement = () => {
+  counterValue -= 1;
+  span.innerHTML = `<span id="value">${counterValue}</span>`;
+};
+
+const increment = () => {
+  counterValue += 1;
+  span.innerHTML = `<span id="value">${counterValue}</span>`;
+};
+
+buttonDecrement.addEventListener("click", decrement);
+buttonIncrement.addEventListener("click", increment);
