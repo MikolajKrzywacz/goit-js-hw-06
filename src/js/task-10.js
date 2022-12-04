a32c87e70f78cd20b2d8bee49ddadc724aac7a83
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2022-12-04 20:03:37 +0100

    Update task-10.js
    deleted commented text

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -6,7 +6,6 @@ const boxesWrapper = document.querySelector("#boxes");
 const btnCreate = document.querySelector("button[data-create]");
 const btnDestroy = document.querySelector("button[data-destroy]");
 const input = document.querySelector("input");
-// console.log(input);
 
 let inputValue = 0;
 
@@ -34,42 +33,3 @@ function destroyBoxes() {
     boxesWrapper.removeChild(boxesWrapper.firstChild);
   }
 }
-
-// (input.addEventListener("input", (ev) => {
-//   inputValue = Number(ev.currentTarget.value);
-//   console.log("creation", inputValue);
-// }))
-
-// const box = document.getElementById("boxes");
-// const input = document.querySelector("input");
-// const creation = document.querySelector("[data-create");
-// const destruction = document.querySelector("[data-destroy]");
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// };
-// let amount = 0;
-// input.addEventListener("input", (ev) => {
-//   amount = Number(ev.currentTarget.value);
-//   console.log("creation", amount);
-// });
-// // destruction.addEventListener("click", (ev) => {
-// //   amount = 0;
-// //   input.value = 0;
-// //   console.log("destr", amount);
-// // });
-// console.log(creation);
-// creation.addEventListener("click", createBoxes);
-// function createBoxes(amount) {
-//   console.log("input:", input.value);
-//   console.log("creating...", typeof amount);
-//   for (let i = 1; i <= amount; i++) {
-//     console.log("creating..");
-//     const newBox = document.createElement("div");
-//     box.append(newBox);
-//     let sizer = 30 + i * 10;
-//     newBox.style.width = `30 + ${sizer} * 10 px`;
-//     newBox.style.height = `30 + ${sizer} * 10 px`;
-//     newBox.style.backgroundColor = getRandomHexColor();
-//     console.log(newBox);
-//   }
-// };
