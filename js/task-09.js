d2eabebb06bbe2e632a66e71852ffe937ee1c8e3
Mikołaj Krzywacz <mikolaj.krzywacz@gmail.com>
2022-12-09 09:24:21 +0100

    Update task-09.js
    added color hex code to span

--- js/task-09.js
+++ js/task-09.js
@@ -2,8 +2,11 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const button = document.querySelector('.change-color');
+const button = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
 
-button.addEventListener('click', () => {
-  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+button.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor = `${color}`;
+  spanColor.textContent = `${color}`;
 });
